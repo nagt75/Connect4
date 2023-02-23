3594e123b438516b956e35703896cf25e0b7bd61
“SandhyaD83” <sandhyadandamudi@gmail.com>
2023-02-23 17:05:19 -0500

    removed class named Connect from js file

--- app.js
+++ app.js
@@ -1,86 +1,80 @@
-class Connect{
-    constructor(i,j){
-      this.parent=document.querySelector(".connect4Box")
-      this.rows=this.parent.children // getting rows as children of connect4Box
-      this.blocks=[];
-      this.j
-      this.i
-      this.k=this.rows.length-1;
-      this.r=[]
-      this.y=[]
-      this.currentPlayer="player1";
-      this.txt=document.querySelector(".txt");
-    }
-
-// adding css style for the blocks
-blockStyle(i,j){
-    for( this.i=i;this.i<this.rows.length;this.i++)
+let parent=document.querySelector(".connect4Box")
+   let rows=parent.children // getting rows as children of connect4Box
+   let blocks=[];
+   let i;
+   let j;
+   let r=[]
+   let y=[]
+   let currentPlayer="player1";
+   let txt;
+   const blockStyle=()=>{
+    for(i=0;i<rows.length;i++)
     {
-     this.blocks=this.rows[this.i].children // getting circle blocks as children of rows
-    for(this.j=j;this.j<this.blocks.length;this.j++){
-        this.blocks[this.j].style.cssText=
+     blocks=rows[i].children // getting circle blocks as children of rows
+    for(j=0;j<blocks.length;j++){
+       blocks[j].style.cssText=
         "height:70px;width:70px; border:solid; blue;border-radius: 50%;background-color:white";// adding styles to blocks
        }
     }
   }
-  player1(i){
-    this.k=5
-    this.i=i;
-    while (this.k >= 0) {
-      this.blocks=this.rows[this.k].children
-      if(this.blocks[this.i].style.backgroundColor=="white"){
-        this.blocks[this.i].style.backgroundColor="red"
-        this.currentPlayer="player2"
-        this.txt.innerHTML=this.currentPlayer
-        this.r.push(this.blocks[this.i])
-        break;
-       }
-      this.k--
-        } 
-        }
-        player2(i){
-            this.i=i;
-            this.k=5
-            while (this.k >= 0) {
-            this.blocks=this.rows[this.k].children
-            if(this.blocks[this.i].style.backgroundColor=="white"){
-              this.blocks[this.i].style.backgroundColor="yellow"
-              this.currentPlayer="player1"
-              this.txt.innerHTML=this.currentPlayer
-              this.y.push(this.blocks[this.i])
-              break;
+  blockStyle();
+  const btnClick=()=>{
+    txt=document.querySelector(".txt")
+    txt.innerHTML=currentPlayer
+    blocks=rows[0].children
+    blocks[0].addEventListener("click",function(){player(0)})
+    blocks[1].addEventListener("click",function(){player(1)})
+    blocks[2].addEventListener("click",function(){player(2)})
+    blocks[3].addEventListener("click",function(){player(3)})
+    blocks[4].addEventListener("click",function(){player(4)})
+    blocks[5].addEventListener("click",function(){player(5)})
+    blocks[6].addEventListener("click",function(){player(6)})
+    }
+    const player=(p)=>{
+        if(currentPlayer=="player1"){
+            player1(p)
             }
-           this.k--
-              }
-          }
-     player(p)  //calling player1 and player2 functions
-      {
-       if(this.currentPlayer=="player1"){
-       this.player1(p)
-       }
-       else if(this.currentPlayer=="player2"){
-         this.player2(p)
-       }
-      }
-      btnClick(){
-      
-        this.txt.innerHTML=this.currentPlayer
-        
-        this.blocks=x.rows[0].children
-        this.blocks[0].addEventListener("click",()=>{this.player(0)})
-        this.blocks[1].addEventListener("click",()=>{this.player(1)})
-        this.blocks[2].addEventListener("click",()=>{this.player(2)})
-        this.blocks[3].addEventListener("click",()=>{this.player(3)})
-        this.blocks[4].addEventListener("click",()=>{this.player(4)})
-        this.blocks[5].addEventListener("click",()=>{this.player(5)})
-        this.blocks[6].addEventListener("click",()=>{this.player(6)})
-        }
+            else if(currentPlayer=="player2"){
+             player2(p)
+            }
 
-
-
-}
-let x= new Connect();
-x.blockStyle(0,0);
-const startGame=()=>{ 
-    x.btnClick()
-}
+    }
+    const player1=(i)=>{
+        j=5
+        while (j >= 0) {
+            blocks=rows[j].children
+              if(blocks[i].style.backgroundColor=="white"){
+                blocks[i].style.backgroundColor="red"
+                r.push(blocks[i].className)//pushing red color blocks into an array
+                document.querySelector(".rednames").append(blocks[i].className)
+                currentPlayer="player2"
+                txt=document.querySelector(".txt")
+                txt.innerHTML=currentPlayer
+                break;
+               }
+              j--
+            } 
+                
+    }
+    const player2=(i)=>{
+        j=5
+        while (j >= 0) {
+            blocks=rows[j].children
+              if(blocks[i].style.backgroundColor=="white"){
+                blocks[i].style.backgroundColor="yellow"
+                y.push(blocks[i].className) ////pushing yellow color blocks into an array
+                document.querySelector(".yellownames").append(blocks[i].className)
+                currentPlayer="player1"
+                txt=document.querySelector(".txt")
+                txt.innerHTML=currentPlayer
+                break;
+               }
+              j--
+            } 
+               
+    }
+    
+    const startGame=()=>{
+        btnClick();
+     
+     }
